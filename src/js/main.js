7ef5d16dac8e12a84c8f2ca133de807d78656e1a
Ray Nathan Low <raynathanlow@gmail.com>
2018-07-29 23:34:31 -0700

    Add original and processed .js files

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// $('body').scroll(function() {
+//   if ($('body').scrollTop() >= $('nav').outerHeight()) {
+//     $('nav').addClass('fixed');
+//     $('nav a').addClass('invert');
+//   } else {
+//     $('nav').removeClass('fixed');
+//     $('nav a').removeClass('invert');
+//   }
+// });
