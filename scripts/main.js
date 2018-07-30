c00ed5716c1ac3b9e49b58ed30b80850cc5b5cab
Ray Nathan Low <raynathanlow@gmail.com>
2018-07-30 00:19:35 -0700

    Remove old scripts folder

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-$("body").scroll(function() {
-  if ($("body").scrollTop() >= $("nav").outerHeight()) {
-    $("nav").addClass("fixed");
-    $("nav a").addClass("invert");
-  } else {
-    $("nav").removeClass("fixed");
-    $("nav a").removeClass("invert");
-  }
-});
