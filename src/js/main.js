8bca053590c1a2c1f2534061595a67bef856e367
Ray Nathan Low <raynathanlow@gmail.com>
2018-08-05 03:40:28 -0700

    Add new styling utilizing diamonds found in logo

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,27 @@
 //     $('nav a').removeClass('invert');
 //   }
 // });
+
+function addBackgroundEffect() {
+  var background = event.target.closest('.project').querySelector('.background');
+  var name = event.target.closest('.project').querySelector('.project-name');
+  background.classList.add('project-hover');
+  name.classList.add('link-underline-hide');
+}
+
+function removeBackgroundEffect() {
+  var background = event.target.closest('.project').querySelector('.background');
+  var name = event.target.closest('.project').querySelector('.project-name');
+  background.classList.remove('project-hover');
+  name.classList.remove('link-underline-hide');
+}
+
+var projects = document.getElementsByClassName('project');
+
+for (var i = 0; i < projects.length; i++) {
+  projects[i].addEventListener('mouseover', addBackgroundEffect, false);
+  projects[i].addEventListener('mouseout', removeBackgroundEffect, false);
+
+  projects[i].addEventListener('focusin', addBackgroundEffect, false);
+  projects[i].addEventListener('focusout', removeBackgroundEffect, false);
+}
